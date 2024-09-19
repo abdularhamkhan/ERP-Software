80f111d32fdc52f4b039472cc3faa3c38f2c7ce5
abdularhamkhanzada <abdularhamkhan02@gmail.com>
2024-09-20 02:04:10 +0500

    setting up auth

--- src/Modules/Auth/AuthLayout.jsx
+++ src/Modules/Auth/AuthLayout.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const AuthLayout = ({ children }) => {
 	return (
 		<div>
-			<h1>chal jao please</h1>
 			{children}
 		</div>
 	);
--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -4,12 +4,9 @@ import Login from "../Modules/Auth/Login";
 const routes = [
 	{
 		path: "/",
-		element: <Home />,  
-	},
-	{
-		path: "/login",
 		element: <Login />,  
 	},
+	
 ];
 
 export default routes;
