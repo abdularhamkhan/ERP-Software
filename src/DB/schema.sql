70b9075f951839fd6a3ca862b0529d2cc1edc6f1
abdularhamkhan <abdularhamkhanzada@gmail.com>
2024-11-28 23:24:38 +0500

    database schema

--- src/DB/schema.sql
+++ src/DB/schema.sql
@@ -8,11 +8,12 @@ CREATE TABLE employees (
     email TEXT NOT NULL UNIQUE,
     phone TEXT NOT NULL,
     address TEXT,
-    dept_id INT REFERENCES departments (dept_id) ON DELETE SET NULL,
     position_id INT REFERENCES positions (position_id) ON DELETE SET NULL,
     CONSTRAINT email_domain_check CHECK (
         email LIKE '%@lhr.nu.edu.pk' OR email LIKE '%@nu.edu.pk'
-    )
+    ),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP
 );
 
 -- 2-Admins
@@ -24,10 +25,12 @@ CREATE TABLE admins (
     email TEXT NOT NULL UNIQUE,
     phone TEXT NOT NULL,
     address TEXT,
-    role TEXT NOT NULL -- e.g., 'CEO', 'CFO', 'COO'
+    role TEXT NOT NULL, -- e.g., 'CEO', 'CFO', 'COO'
     CONSTRAINT email_domain_check CHECK (
         email LIKE '%@lhr.nu.edu.pk' OR email LIKE '%@nu.edu.pk'
-    )
+    ),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP
 );
 
 -- 3-Hr Manager
@@ -36,24 +39,30 @@ CREATE TABLE hr (
     email TEXT NOT NULL UNIQUE,
     CONSTRAINT email_domain_check CHECK (
         email LIKE '%@lhr.nu.edu.pk' OR email LIKE '%@nu.edu.pk'
-    )
+    ),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP
 );
 
 -- 4-Attendance
 CREATE TABLE attendance (
     attendance_id SERIAL PRIMARY KEY,
     employee_id INT REFERENCES employees (employee_id) ON DELETE CASCADE,
-    date DATE NOT NULL,
-    status TEXT NOT NULL CHECK (status IN ('Present', 'Absent', 'Leave', 'Late'))
+    date DATE NOT NULL CHECK (date <= CURRENT_DATE),
+    status TEXT NOT NULL CHECK (status IN ('Present', 'Absent', 'Leave', 'Late')),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP
 );
 
 -- 5-Leaves
 CREATE TABLE leaves (
     leave_id SERIAL PRIMARY KEY,
     employee_id INT REFERENCES employees (employee_id) ON DELETE CASCADE,
-    date DATE NOT NULL,
+    date DATE NOT NULL CHECK (date >= CURRENT_DATE),
     reason TEXT DEFAULT 'emergency' CHECK (reason IN ('emergency', 'sick', 'travel')),
-    status TEXT NOT NULL CHECK (status IN ('approved', 'not-approved'))
+    status TEXT NOT NULL DEFAULT 'not-approved' CHECK (status IN ('approved', 'not-approved')),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP
 );
 
 -- 6-Appraisals
@@ -61,20 +70,21 @@ CREATE TABLE appraisals (
     appraisal_id SERIAL PRIMARY KEY,
     employee_id INT REFERENCES employees (employee_id) ON DELETE CASCADE,
     appraisal_date TIMESTAMP DEFAULT NOW(), -- Automatically fills the current date and time
-    year INT GENERATED ALWAYS AS (EXTRACT(YEAR FROM appraisal_date)::INT) STORED,
-    month INT GENERATED ALWAYS AS (EXTRACT(MONTH FROM appraisal_date)::INT) STORED,
     self_appraisal NUMERIC(3, 2) CHECK (self_appraisal BETWEEN 0 AND 5),
-    hr_appraisal NUMERIC(3, 2) CHECK (hr_appraisal BETWEEN 0 AND 5)
+    hr_appraisal NUMERIC(3, 2) CHECK (hr_appraisal BETWEEN 0 AND 5),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP
 );
 
 -- 7-Salaray
 CREATE TABLE salaries (
     salary_id SERIAL PRIMARY KEY,
     employee_id INT REFERENCES employees (employee_id) ON DELETE CASCADE,
-    month INT NOT NULL CHECK (month BETWEEN 1 AND 12),
-    year INT NOT NULL,
+    salary_date TIMESTAMP DEFAULT NOW(), -- Automatically fills the current date and time
     salary_amount NUMERIC(10, 2) NOT NULL,
-    bonus NUMERIC(10, 2) DEFAULT 0
+    bonus NUMERIC(10, 2) DEFAULT 0,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP
 );
 
 
@@ -84,12 +94,14 @@ CREATE TABLE salaries (
 CREATE TABLE loan (
     loan_id SERIAL PRIMARY KEY,
     employee_id INT REFERENCES employees (employee_id) ON DELETE CASCADE,
-    loan_application_number TEXT NOT NULL UNIQUE,
+    loan_application_number UUID DEFAULT gen_random_uuid() NOT NULL UNIQUE,
     loan_application_pdf TEXT, -- URL to the PDF in Supabase storage
     approval_status TEXT NOT NULL CHECK (approval_status IN ('pending', 'approved', 'cancel')),
     total_amount NUMERIC(10, 2) NOT NULL,
     return_amount NUMERIC(10, 2) NOT NULL, -- Monthly return amount
-    reason TEXT NOT NULL CHECK (reason IN ('emergency', 'planned', 'study'))
+    reason TEXT NOT NULL CHECK (reason IN ('emergency', 'planned', 'study')),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP
 );
 
 
@@ -97,12 +109,14 @@ CREATE TABLE loan (
 CREATE TABLE insurance (
     insurance_id SERIAL PRIMARY KEY,
     employee_id INT REFERENCES employees (employee_id) ON DELETE CASCADE,
-    insurance_application_number TEXT NOT NULL UNIQUE,
+    insurance_application_number UUID DEFAULT gen_random_uuid() NOT NULL UNIQUE,
     insurance_application_pdf TEXT, -- URL to the PDF in Supabase storage
     approval_status TEXT NOT NULL CHECK (approval_status IN ('pending', 'approved', 'cancel')),
     total_amount NUMERIC(10, 2) NOT NULL,
     deposit_amount NUMERIC(10, 2) NOT NULL, -- Monthly deposit amount
-    type TEXT NOT NULL CHECK (type IN ('health', 'life', 'home', 'car', 'wedding'))
+    type TEXT NOT NULL CHECK (type IN ('health', 'life', 'home', 'car', 'wedding')),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP
 );
 
 
@@ -110,12 +124,17 @@ CREATE TABLE insurance (
 CREATE TABLE departments (
     dept_id SERIAL PRIMARY KEY,
     dept_name TEXT NOT NULL UNIQUE,
-    dept_lead INT REFERENCES employees (employee_id) ON DELETE SET NULL
+    dept_lead INT REFERENCES employees (employee_id) ON DELETE SET NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP
 );
 
 -- 11-Positions Table
 CREATE TABLE positions (
     position_id SERIAL PRIMARY KEY,
     position_name TEXT NOT NULL UNIQUE,
-    dept_id INT REFERENCES departments (dept_id) ON DELETE CASCADE
+    dept_id INT REFERENCES departments (dept_id) ON DELETE SET NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP
+
 );
